3b0d8334fd92a2cb3c7769b096b242db96177f19
Ale Benson <alebenson@gmail.com>
2013-10-05 19:43:00 -0300

    Create index.php

---
+++ example/index.php
@@ -0,0 +1,8 @@
+<?php include dirname(__FILE__) . DIRECTORY_SEPARATOR . 'includes/functions.php' ?>
+
+<?php plugin('header') ?>
+
+<!-- Add your site or application content here -->
+<p>Hello world! This is HTML5 Boilerplate.</p>
+
+<?php plugin('footer') ?>
