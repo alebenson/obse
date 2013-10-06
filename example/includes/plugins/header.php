2d97ed26d969750ca326196f04a88f9fbdae29f2
Ale Benson <alebenson@gmail.com>
2013-10-05 21:12:12 -0300

    Update header.php

--- example/includes/plugins/header.php
+++ example/includes/plugins/header.php
@@ -1,6 +1,21 @@
-<!DOCTYPE html>
+<?php include dirname(__FILE__) . DIRECTORY_SEPARATOR . 'includes/functions.php' ?>
 
-<?php plugin('ie_conditional_comments') ?>
-<?php plugin('head') ?>
-<body>
-<?php plugin('browsehappy') ?>
+<?php plugin('header') ?>
+<section class="wrapper">
+	<header>
+		<h1>Home</h1>
+		<!-- Add your site or application content here -->
+		<p><em>Hello world! This is <strong>obse</strong>.</em></p>
+	</header>
+
+	<div class="main">
+
+		<?php plugin('home_news') ?>
+	</div>
+
+	<div class="sidebar">
+		<?php plugin('twitter_widget') ?>
+	</div>
+</section>
+
+<?php plugin('footer') ?>
